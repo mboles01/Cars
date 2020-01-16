11ba1218bd6d667e56446482d9e46b917703c04f
Michael Boles <michaelboles@Michaels-MacBook-Pro.local>
2020-01-16 09:21:59 -0800

    Uploaded used car data

--- scripts/scrape_web.py
+++ scripts/scrape_web.py
@@ -16,63 +16,74 @@ import requests
 from user_agent import generate_user_agent
 import time
 import numpy.random as npr
-import csv
 import pandas as pd
-
 
 ### SCRAPE AUTOTRADER ###
 
+
+
 # stipulate search parameters
-baseurl = 'https://www.autotrader.com/cars-for-sale/Used+Cars/'
-location = 'Burlingame+CA-94010?'
-listingtype = 'listingTypes=USED&' # listingTypes=USED%2CNEW&
-radius = 'searchRadius=100&'
-zipcode = 'zip=94010&'
-includenew = 'marketExtension=include&isNewSearch=true&'
-sorting = 'sortBy=relevance&numRecords=100&'
+baseurl = 'https://www.autotrader.com/cars-for-sale/Certified+Cars/'
+location = 'San+Francisco+CA-94107?'
+listingtype = 'listingTypes=NEW&' # 'listingTypes=CERTIFIED%2CUSED&' # 'listingTypes=USED%2CCERTIFIED%2CNEW&' # listingTypes=USED%2CNEW& 'listingTypes=USED&'
+radius = 'searchRadius=100&' # 'searchRadius=300&'
+zipcode = 'zip=94107&'
+includenew = 'marketExtension=include&' #'marketExtension=include&isNewSearch=true&'
+# style = 'vehicleStyleCodes=SEDAN&'
+isnew = 'isNewSearch=false&'
+sorting = 'sortBy=distanceASC&' # 'sortBy=mileageASC&' 'sortBy=yearDESC&'
+number = 'numRecords=100&'
 firstrecord = 'firstRecord='
 
-for index in range(1000,2000,100):
-    
-    # build in wait time
-    wait_time = npr.randint(10,25)
-    time.sleep(wait_time)
+for year in range(2010,2021,1):
     
     # print status update
-    print('Scraping data for page with first listing index = %s ' % index)
+    print('Scraping %s' % year)
+    
+    yearmade = str(year) + '/'
+    startyear = 'startYear=' + str(year) + '&'
+    endyear = 'endYear=' + str(year) + '&'
 
-    # compile full URL
-    url = baseurl + location + listingtype + radius + zipcode + includenew + sorting + firstrecord + str(index)
+    for index in range(0,100,100):
+        
+        # compile full URL
+        url = baseurl + yearmade + location + listingtype + radius + zipcode + startyear + includenew + endyear + isnew + sorting + number + firstrecord + str(index)        
+        
+        # print status update
+        print('Page %s' % str(int(index/100+1)))
+        print(url)
     
-    # get homepage session
-    session = requests.Session()
-    headers = {'User-Agent':'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:72.0) Gecko/20100101 Firefox/72.0'}
+        # build in wait time
+        wait_time = npr.randint(1,3)
+        time.sleep(wait_time)
     
-    # parse content
-    homepage = session.get(url, headers=headers)
-    tree = html.fromstring(homepage.content)
-    
-    # loop over all entries and collect this data
-    listings = []
-    for item in range(0,103):
-        xpath = '/html/body/div[1]/div/div[2]/div[2]/div/div[2]/div[1]/div[2]/div[3]/div/div[2]/script[' + str(item) + ']/text()'
-        listing_individual = tree.xpath(xpath)
-        listings.append(listing_individual)
-    
-    # flatten list of lists
-    listings = [item for sublist in listings for item in sublist]
-    
-    # # split on commas
-    # newlist = []
-    # for word in listings:
-    #     word = word.split(",")
-    #     newlist.extend(word)  # <----
-    
-    # create pandas dataframe
-    listings_df = pd.DataFrame(listings)
-    
-    # save each batch of 100 listings
-    listings_df.to_csv('listings_' + str(index) + '_to_' + str(index+100))
+        # get homepage session
+        session = requests.Session()
+        headers = {'User-Agent':'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:72.0) Gecko/20100101 Firefox/72.0'}
+        
+        # parse content
+        homepage = session.get(url, headers=headers)
+        tree = html.fromstring(homepage.content)
+        
+        # count number of results
+        results = tree.xpath('//div[@class="results-text-container text-size-200"]//text()')
+        print(results)
+        
+        # loop over all entries and collect this data
+        listings = []
+        for item in range(0,103):
+            xpath = '/html/body/div[1]/div/div[2]/div[2]/div/div[2]/div[1]/div[2]/div[3]/div/div[2]/script[' + str(item) + ']/text()'
+            listing_individual = tree.xpath(xpath)
+            listings.append(listing_individual)
+        
+        # flatten list of lists
+        listings = [item for sublist in listings for item in sublist]
+        
+        # create pandas dataframe
+        listings_df = pd.DataFrame(listings)
+        
+        # save each batch of 100 listings
+        listings_df.to_csv('../data/raw/listings_' + str(year) + '_' + str(int(index/100+1)))
 
 
 
