231cb3e0a12b9793f0ac43e1528030adac368c1f
Michael Boles <michaeladamboles@gmail.com>
2020-01-18 13:28:51 -0800

    Updated listings

--- scripts/clean_data.py
+++ scripts/clean_data.py
@@ -10,42 +10,43 @@ Created on Thu Jan 16 09:35:05 2020
 import os
 os.chdir('/Users/michaelboles/Michael/Coding/2020/Insight/Project/Depreciator/scripts') 
 
-# # point to directory containing many raw data files
-# import os.path
-# import glob
-# mypath = r'../data/raw/Used_1980-2020'
-# all_files = glob.glob(mypath + "/*.csv")
+# point to directory containing many raw data files
+import os.path
+import glob
+mypath = r'../data/raw/New_2017-2020' # r'../data/raw/Used_1980-2020'
 
-# # load all csv files into one pandas dataframe (concatenating)
-# import pandas as pd
-# data_raw = pd.concat((pd.read_csv(f) for f in all_files))
+all_files = glob.glob(mypath + "/*.csv")
 
-# # loop through each line in raw data
-# import ast
-# from helper_functions import flatten
+# load all csv files into one pandas dataframe (concatenating)
+import pandas as pd
+data_raw = pd.concat((pd.read_csv(f) for f in all_files))
 
-# data_df = pd.DataFrame()
-# for line in data_raw['0'].iloc[:-1]:
+# loop through each line in raw data
+import ast
+from helper_functions import flatten
+
+data_df = pd.DataFrame()
+for line in data_raw['0'].iloc[:-1]:
     
-#     # convert string to dictionary
-#     line_dict = ast.literal_eval(line)
+    # convert string to dictionary
+    line_dict = ast.literal_eval(line)
     
-#     # flatten dictionary
-#     line_flat = flatten(line_dict)
+    # flatten dictionary
+    line_flat = flatten(line_dict)
     
-#     # convert to dictionary
-#     line_flat_df = pd.DataFrame(line_flat, index=[0])
+    # convert to dictionary
+    line_flat_df = pd.DataFrame(line_flat, index=[0])
     
-#     # append to existing dataframe
-#     data_df = data_df.append(line_flat_df, ignore_index=True)
+    # append to existing dataframe
+    data_df = data_df.append(line_flat_df, ignore_index=True)
     
-# # save dataframe as .csv
-# data_df.to_csv('../data/clean/listings_clean_1.csv')
+# save dataframe as .csv
+data_df.to_csv('../data/clean/new_listings_clean_1.csv')
 
 
 # open clean_1 dataframe
 import pandas as pd
-listings_clean1 = pd.read_csv('../data/clean/listings_clean_1.csv')
+listings_clean1 = pd.read_csv('../data/clean/new_listings_clean_1.csv')
 
 # drop columns 
 listings_clean1.columns
@@ -59,8 +60,6 @@ listings_clean2 = listings_clean1.drop(columns=['Unnamed: 0',
                                                 'offers_itemCondition',
                                                 'offers_priceCurrency'])
 
-listings_small = listings_clean2[:10]
-
 # reorder columns
 
 cols2 = ['productionDate', 
@@ -68,22 +67,22 @@ cols2 = ['productionDate',
          'model', 
          'mileageFromOdometer_value', 
          'offers_price', 
-         'vehicleIdentificationNumber',  
+         'bodyType',
          'color', 
          'vehicleInteriorColor', 
          'driveWheelConfiguration', 
          'vehicleTransmission', 
-         'vehicleEngine', 
-         'bodyType', 
+         'vehicleEngine',  
          'name', 
-         'url', 
+         'url',
+         'vehicleIdentificationNumber',
          'description']
 
 listings_clean3 = listings_clean2[cols2]
 
 # rename columns
 
-listings_clean3.rename(columns={'productionDate': 'Year', 
+listings_clean4 = listings_clean3.rename(columns={'productionDate': 'Year', 
                                 'brand': 'Make',
                                 'model': 'Model',
                                 'mileageFromOdometer_value': 'Mileage',
@@ -97,11 +96,27 @@ listings_clean3.rename(columns={'productionDate': 'Year',
                                 'bodyType': 'Body',
                                 'name': 'ListTitle',
                                 'url': 'URL',
-                                'description': 'Description'})
+                                'description': 'Description'})  
 
 
+# save dataframe as .csv
+listings_clean4.to_csv('../data/clean/listings_clean_4.csv')
 
 
+# plot histogram
+prices = listings_clean4['Price']
+
+import numpy as np
+prices_mean = round(np.average(prices))
+prices_median = np.median(prices)
+prices_stdev = np.std(prices)
+
+
+from plotfunctions import plothist
+binwidth = 1000
+
+prices_textbox = 'Average = $%.0f \nMedian = $%.0f \nStdev = $%.0f' % (prices_mean, prices_median, prices_stdev)
+plothist(prices, binwidth, prices_textbox, 0, 70000, 'Price ($)', 'Counts', 'Prices.png')
 
 
 
---
+++ scripts/plotfunctions.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 27 14:14:07 2019
+
+@author: michaelboles
+"""
+
+# import packages
+import numpy as np
+import matplotlib.pyplot as plt
+
+# plot histogram
+
+def plothist(data, binwidth, textbox, xmin, xmax, xlabel, ylabel, figure_name):
+    fig, ax = plt.subplots(1,1,figsize=(7,7))
+    bins = np.arange(round(min(data),1), max(data) + binwidth, binwidth)
+    props = dict(facecolor='white', alpha=1.0)
+
+    ax.hist(data, bins, edgecolor = 'black', facecolor = 'blue')
+    
+    plt.xlim(xmin, xmax); plt.xlabel(xlabel, fontsize = 18, fontname = 'Helvetica')
+    plt.ylabel(ylabel, fontsize = 18)
+    ax.tick_params(axis = 'x', labelsize = 14); ax.tick_params(axis = 'y', labelsize = 14)
+    ax.text(0.575, 0.97, textbox, transform = ax.transAxes, fontsize = 18, fontname = 'Helvetica', verticalalignment = 'top', bbox = props)
+    
+    for tick in ax.get_xticklabels():
+        tick.set_fontname('Helvetica')
+    for tick in ax.get_yticklabels():
+        tick.set_fontname('Helvetica')
+    
+    plt.rcParams['axes.unicode_minus'] = False
+    plt.grid(); ax.set_axisbelow(True)
+    plt.savefig(figure_name, dpi = 600)
+    plt.show()
+    
+    
+def plothist2(data1, data2, binwidth, textbox, xmin, xmax, xlabel, ylabel, figure_name):
+    fig, ax = plt.subplots(1,1,figsize=(7,7))
+    bins = np.arange(round(min(data1),1), max(data1) + binwidth, binwidth)
+    props = dict(facecolor='white', alpha=1.0)
+    
+    ax.hist(data2, bins, edgecolor = 'black', facecolor = 'gray', alpha = 0.5)
+    ax.hist(data1, bins, edgecolor = 'black', facecolor = 'blue', alpha = 0.5)
+        
+    plt.xlim(xmin, xmax); plt.xlabel(xlabel, fontsize = 18, fontname = 'Helvetica')
+    plt.ylabel(ylabel, fontsize = 18)
+    ax.tick_params(axis = 'x', labelsize = 14); ax.tick_params(axis = 'y', labelsize = 14)
+#    ax.text(0.575, 0.97, textbox, transform = ax.transAxes, fontsize = 18, fontname = 'Helvetica', verticalalignment = 'top', bbox = props)
+    
+    for tick in ax.get_xticklabels():
+        tick.set_fontname('Helvetica')
+    for tick in ax.get_yticklabels():
+        tick.set_fontname('Helvetica')
+    
+    plt.rcParams['axes.unicode_minus'] = False
+    ax.rcParams['axes.grid'] = False
+    plt.grid(); ax.set_axisbelow(True)
+    plt.savefig(figure_name, dpi = 600)
+    plt.show()
+
+
+#def plothist2(data, binwidth, xmin, xmax, xlabel, ylabel):
+#    fig, ax = plt.subplots(1,1,figsize=(7,7))
+#    bins = np.arange(round(min(data),1), max(data) + binwidth, binwidth)
+#
+#    ax.hist(data, bins, edgecolor = 'black', facecolor = 'blue')
+#    
+#    plt.xlim(xmin, xmax); plt.xlabel(xlabel, fontsize = 18, fontname = 'Helvetica')
+#    plt.ylabel(ylabel, fontsize = 18)
+#    ax.tick_params(axis = 'x', labelsize = 14); ax.tick_params(axis = 'y', labelsize = 14)
+#    
+#    for tick in ax.get_xticklabels():
+#        tick.set_fontname('Helvetica')
+#    for tick in ax.get_yticklabels():
+#        tick.set_fontname('Helvetica')
+#    
+#    plt.rcParams['axes.unicode_minus'] = False
+#    plt.grid(); ax.set_axisbelow(True)
+#    plt.show()
