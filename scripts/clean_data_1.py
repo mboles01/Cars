ff06af777800e289b3cb96d55c3d20e170db9ed2
Michael Boles <michaeladamboles@gmail.com>
2020-01-20 09:06:27 -0800

    Exploratory analysis

---
+++ scripts/clean_data_1.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 16 09:35:05 2020
+
+@author: michaelboles
+"""
+
+# set up working directory
+import os
+os.chdir('/Users/michaelboles/Michael/Coding/2020/Insight/Project/Depreciator/scripts') 
+
+# point to directory containing many raw data files
+import os.path
+import glob
+mypath = r'../data/raw/New_2017-2020' # r'../data/raw/Used_1980-2020'
+
+all_files = glob.glob(mypath + "/*.csv")
+
+# load all csv files into one pandas dataframe (concatenating)
+import pandas as pd
+data_raw = pd.concat((pd.read_csv(f) for f in all_files))
+
+# loop through each line in raw data
+import ast
+from helper_functions import flatten
+
+data_df = pd.DataFrame()
+for line in data_raw['0'].iloc[:-1]:
+    
+    # convert string to dictionary
+    line_dict = ast.literal_eval(line)
+    
+    # flatten dictionary
+    line_flat = flatten(line_dict)
+    
+    # convert to dictionary
+    line_flat_df = pd.DataFrame(line_flat, index=[0])
+    
+    # append to existing dataframe
+    data_df = data_df.append(line_flat_df, ignore_index=True)
+    
+# save dataframe as .csv
+data_df.to_csv('../data/clean/new_listings_clean_1.csv')
+
+
